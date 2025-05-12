1791d42d89d98882c2d42c0013a606a596037966
Emil <emjo0002@stud.kea.dk>
2025-05-12 09:26:30 +0200

    .

--- src/app/componets/Hero.jsx
+++ src/app/componets/Hero.jsx
@@ -59,46 +59,48 @@ export default function Hero() {
   }, []);
 
   return (
-    <section className="h-screen relative px-[clamp(4rem,12vw,20rem)] border-b border-black flex items-center justify-center">
+    <section className="h-screen relative px-[clamp(4rem,12vw,20rem)] border-black flex items-center justify-center">
       <div
         ref={containerRef}
-        className="relative w-full max-w-3xl h-[400px]"
+        className="relative w-full max-w-[1600px] h-[600px]"
       >
         <Image
-          src="https://picsum.photos/250/350?grayscale&random=4"
-          alt="Random 4"
-          width={250}
-          height={350}
-          className="absolute -top-10 right-25 shadow-lg z-0 object-cover collage-img"
-        />
-        <Image
-          src="https://picsum.photos/400/300?grayscale&random=3"
-          alt="Random 3"
-          width={400}
-          height={300}
-          className="absolute bottom-5 left-30 shadow-lg z-10 object-cover collage-img"
-        />
-        <Image
-          src="https://picsum.photos/275/315?grayscale&random=2"
-          alt="Random 2"
-          width={275}
-          height={315}
-          className="absolute -top-20 left-40 shadow-lg z-20 object-cover collage-img"
-        />
-        <Image
-          src="https://picsum.photos/350/325?grayscale&random=5"
-          alt="Random 5"
-          width={350}
-          height={325}
-          className="absolute bottom-0 right-0 shadow-lg z-25 object-cover collage-img"
-        />
-        <Image
-          src="https://picsum.photos/250/250?grayscale&random=1"
+          src="https://picsum.photos/350/480?grayscale&random=1"
           alt="Random 1"
-          width={250}
-          height={250}
-          className="absolute top-18 left-0 shadow-lg z-30 object-cover collage-img"
+          width={350}
+          height={480}
+          className="absolute top-15 left-0 z-0 object-cover collage-img"
         />
+         <Image
+          src="https://picsum.photos/430/280?grayscale&random=5"
+          alt="Random 5"
+          width={430}
+          height={280}
+          className="absolute top-5 right-10 z-0 object-cover collage-img"
+        />
+        <Image
+          src="https://picsum.photos/300/280?grayscale&random=4"
+          alt="Random 4"
+          width={300}
+          height={280}
+          className="absolute bottom-5 right-0 z-0 object-cover collage-img"
+        />
+        <Image
+          src="https://picsum.photos/499/300?grayscale&random=3"
+          alt="Random 3"
+          width={499}
+          height={300}
+          className="absolute bottom-0 left-87.5 z-0 object-cover collage-img"
+        />
+        <Image
+          src="https://picsum.photos/330/300?grayscale&random=2"
+          alt="Random 2"
+          width={330}
+          height={300}
+          className="absolute top-0 left-87.5 z-0 object-cover collage-img"
+        />
+       
+        
         
         
         
