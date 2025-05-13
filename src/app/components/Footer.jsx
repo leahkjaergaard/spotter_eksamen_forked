bcd8e1ce75d88997be04b4bd93e2bcc781f550b6
Emil <emjo0002@stud.kea.dk>
2025-05-13 17:10:07 +0200

    .

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Footer() {
+    return (
+        <section>
+            <div className="w-full">
+                <h1 className="text-[clamp(1rem,21vw,42rem)] font-bold text-center">SPOTTER</h1>
+            </div>
+        </section>
+    )
+}
