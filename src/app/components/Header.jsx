b561be0083de5bd5e45322e50a133e56c1045428
Leah <lepa0001@stud.kea.dk>
2025-05-16 10:43:26 +0200

    header

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,29 @@
+"use client";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="w-full py-6 px-6 flex justify-between items-center text-black font-medium text-lg">
+      {/* Venstre links */}
+      <div className="flex gap-6">
+        <Link href="/productlist" className="hover:underline">
+          Produkter
+        </Link>
+        <Link href="/om-os" className="hover:underline">
+          Om os
+        </Link>
+        <Link href="/psykiatrifonden" className="hover:underline">
+          Psykiatrifonden
+        </Link>
+        <Link href="/contact" className="hover:underline">
+          Kontakt
+        </Link>
+      </div>
+
+      {/* Logo i midten */}
+      <div className="absolute left-1/2 transform -translate-x-1/2 font-bold italic tracking-[-0.08em] text-2xl">
+        <Link href="/">Spotter.</Link>
+      </div>
+    </header>
+  );
+}
--- src/app/psykiatrifonden/page.js
+++ src/app/psykiatrifonden/page.js
@@ -1,8 +1,10 @@
 import PsykiatriFonden from "@/app/components/PsykiatriFonden";
+import Header from "@/app/components/Header";
 
 export default function PsykiatriPage() {
   return (
     <main>
+      <Header />
       <PsykiatriFonden />
     </main>
   );
