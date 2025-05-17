506ff5196e6ac313d3fccb47960404f13b08048a
Leah <lepa0001@stud.kea.dk>
2025-05-17 19:28:58 +0200

    .

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -20,7 +20,7 @@ export default function ProductListPage() {
   return (
     <>
       <Header />
-      ayout
+
       <Basket ref={basketRef} />
       {basketReady && basketRef.current && <GetProducts openBasket={basketRef.current.openBasket} addToBasket={basketRef.current.addItem} />}
       <BurgerMenu />
